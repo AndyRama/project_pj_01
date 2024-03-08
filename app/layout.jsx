8206902dc8711a28c7abd719de9f96b74540f083
Andy Ramaroson <andyramaroson@gmail.com>
2024-03-08 15:53:44 +0100

    (setup) add prettier and config jsdoc | create navigation component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,17 +1,23 @@
-import { Inter } from "next/font/google";
-import "./globals.scss";
+import { Inter } from 'next/font/google'
+import './globals.scss'
 
-const inter = Inter({ subsets: ["latin"] });
+import Navigation from './components/Navigation'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: "Unlcoaching.com",
-  description: "Allier entraînement rigoureux et nutrition équilibrée pour un mode de vie sain et durable.",
-};
+  title: 'Unlcoaching.com',
+  description:
+    'Allier entraînement rigoureux et nutrition équilibrée pour un mode de vie sain et durable.',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navigation />
+        {children}
+      </body>
     </html>
-  );
+  )
 }
