1df67cf5db06c174234754245e74970a907e52aa
Andy Ramaroson <andyramaroson@gmail.com>
2024-03-14 17:27:57 +0100

    (feat)[Home] create component CardDescriptionR

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Hero from './components/Hero'
 import CardDescriptionL from './components/CardDescriptionL'
+import CardDescriptionR from './components/CardDescriptionR'
 import RecentBlog from './components/RecentBlog'
 import SubHero from './components/about/SubHero'
 export default function Home() {
   return (
     <>
       <Hero className="pt-32 pb-10 lg-pb:20" />
-      <CardDescriptionL className=" bg-[#1D1D1D]" />
+      <CardDescriptionL className=" pt-14 pb-15 bg-[#1D1D1D]" />
+      <CardDescriptionR className=" pt-14 pb-15 bg-[#1D1D1D]" />
       <RecentBlog className="pt-14 pb-15 lg:pb-32" />
       <SubHero className="pt-52" />
     </>
