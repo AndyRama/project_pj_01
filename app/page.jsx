978fb189990ec3e6cd03ad1c6b88369b7ec01ca4
Andy Ramaroson <andyramaroson@gmail.com>
2024-03-22 00:01:29 +0100

    (feat)[Home] cardProgram component skip

--- app/page.jsx
+++ app/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
       <CardDescriptionR className=" pt-14 pb-15 sm:bg-[#1D1D1D]" />
       <CardGridImage />
       <CardDescription className="pb-15 bg-[#1D1D1D]" />
-      <CardProgram />
+      {/* <CardProgram /> */}
       <RecentBlog className="pb-5 lg:pb-32" />
       <Subscribe className="py-16 pt-64 -mt-48 lg:py-32 bg-[#1D1D1D]" />
     </>
