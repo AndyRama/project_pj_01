3320c726c867e9710f62916447e0c6b5ca094fd4
Andy Ramaroson <andyramaroson@gmail.com>
2024-03-31 15:06:08 +0200

    (feat)[pricing] create card for  Pricing page

--- app/components/Pricing.jsx
+++ app/components/Pricing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const pricingOptions = [
   {
-    title: 'Programmation niveau',
+    title: 'Pack Niveau',
     subtitle: 'Débutant(4 Mois)',
     price: '150€',
     subPrice: '(3 * 150€ / 3 mois) ',
@@ -16,7 +16,7 @@ const pricingOptions = [
     ],
   },
   {
-    title: 'Programmation niveau',
+    title: 'Pack Niveau',
     subtitle: 'Intermerdaire (4 Mois)',
     price: '150€',
     subPrice: '(3 * 150€ / 3 mois) ',
@@ -29,7 +29,7 @@ const pricingOptions = [
     ],
   },
   {
-    title: 'Programmation niveau',
+    title: 'Pack Niveau',
     subtitle: 'Confirmé (4 Mois)',
     price: '150€',
     subPrice: '(3 * 150€ / 3 mois) ',
@@ -42,7 +42,7 @@ const pricingOptions = [
     ],
   },
   {
-    title: 'Programmation niveau',
+    title: 'Pack Niveau',
     subtitle: 'Débutant(1 Mois)',
     price: '150€',
     subPrice: '(1 * 150€ / 1 mois) ',
@@ -51,11 +51,11 @@ const pricingOptions = [
       'Programme de musculation personalisé en fonction de tes objectifs.',
       'Plan alimentaire adapté à tes besoins',
       'Accompagnement sur les divers TCA ou problématique de santé.',
-      'Bilan mensuel par email après 4 semaines',
+      'Bilan mensuel par email après 4 semaines pour le suivie.',
     ],
   },
   {
-    title: 'Programmation niveau',
+    title: 'Pack Niveau',
     subtitle: 'Intermerdaire (1 Mois)',
     price: '150€',
     subPrice: '(1 * 150€ / 1 mois) ',
@@ -68,7 +68,7 @@ const pricingOptions = [
     ],
   },
   {
-    title: 'Programmation niveau',
+    title: 'Pack Niveau',
     subtitle: 'Confirmé (1 Mois)',
     price: '150€',
     subPrice: '(1 * 150€ / 1 mois) ',
@@ -84,10 +84,9 @@ const pricingOptions = [
 
 const EbookOptions = [
   {
-    title: 'Programmation niveau',
-    subtitle: 'Débutant(4 Mois)',
-    price: '150€',
-    subPrice: '(3 * 150€ / 3 mois) ',
+    title: 'Ebook1',
+    subtitle: 'Sujet numero 1',
+    price: 'Free',
     features: [
       'Pack adapter aux débutants en musculation.',
       'Programme de musculation personalisé en fonction de tes objectifs.',
@@ -98,10 +97,9 @@ const EbookOptions = [
     ],
   },
   {
-    title: 'Programmation niveau',
-    subtitle: 'Débutant(4 Mois)',
-    price: '150€',
-    subPrice: '(3 * 150€ / 3 mois) ',
+    title: 'Ebook2',
+    subtitle: 'Sujet numero 2',
+    price: '15€',
     features: [
       'Pack adapter aux débutants en musculation.',
       'Programme de musculation personalisé en fonction de tes objectifs.',
@@ -113,8 +111,58 @@ const EbookOptions = [
   },
 ]
 
-function Pricing() {
-  return <div>Pricing</div>
+const Pricing = () => {
+  return (
+    <section className="mt-20 mb-20">
+      <div className="container px-4 mx-auto">
+        {/* Main Card Pricing - content  */}
+        <div className="flex flex-wrap">
+          {pricingOptions.map((option, index) => (
+            <div key={index} className="w-full sm:w-1/2 lg:w-1/3 p-2">
+              <div className="p-10 border border-white rounded-xl">
+                {/* Card Pricing - Header - Title */}
+                <p className="text-4xl mb-1 text-center text-white">
+                  {option.title}
+                </p>
+                {/* Card Pricing - Header - subTitle */}
+                <p className="text-xl mb-6 text-orange-400 text-center">
+                  {option.subtitle}
+                </p>
+                {/* Card Pricing - Header - Price  */}
+                <p className="mb-1 text-center">
+                  <span className="text-4xl text-white mt-6 mr-2">
+                    {option.price}
+                  </span>
+                  <span className="text-white tracking-tight">/Mois</span>
+                </p>
+                {/* Card Pricing - Header - subPrice  */}
+                <p className="text-xl mb-6 text-orange-400 text-center">
+                  {option.subPrice}
+                </p>
+                {/* Card Pricing - main - contents */}
+                <ul>
+                  {option.features.map((feature, index) => (
+                    <li
+                      key={index}
+                      className="mt-8 flex items-center text-orange-400"
+                    >
+                      •<span className="ml-2 text-white">{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+                <a
+                  href="#"
+                  className="inline-flex justify-center items-center text-white text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-orange-400 border border-orange-400 rounded-lg transition duration-200"
+                >
+                  Choisir le plan
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Pricing
--- app/prestations/page.jsx
+++ app/prestations/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SubHero from '../components/about/SubHero'
 import Subscribe from '@/app/components/Subscribe'
+import Pricing from '@/app/components/Pricing'
 
 export const metadata = {
   title: 'Prestations Unlcoaching',
@@ -10,6 +11,7 @@ const page = () => {
   return (
     <>
       <SubHero className="pt-32" title="Prestations" />
+      <Pricing className="mt-20 mb-20" />
       <Subscribe className="py-16 pt-64 lg:py-32 bg-orange-600" />
     </>
   )
