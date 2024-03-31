9b33dc5f3c2e0586954dca0138b3bb26bbe81ba7
Andy Ramaroson <andyramaroson@gmail.com>
2024-03-31 14:27:27 +0200

    (feat)[pricing] create data for Pricing card

---
+++ app/components/Pricing.jsx
@@ -0,0 +1,120 @@
+import React from 'react'
+
+const pricingOptions = [
+  {
+    title: 'Programmation niveau',
+    subtitle: 'Débutant(4 Mois)',
+    price: '150€',
+    subPrice: '(3 * 150€ / 3 mois) ',
+    features: [
+      'Pack adapter aux débutants en musculation.',
+      'Programme de musculation personalisé en fonction de tes objectifs.',
+      'Plan alimentaire adapté à tes besoins',
+      'Accompagnement sur les divers TCA ou problématique de santé.',
+      'Bilan mensuel photo + mensuration avec un suivi plus approfondie',
+      'Communication whatsApp & Email + visio ou call chaque mois',
+    ],
+  },
+  {
+    title: 'Programmation niveau',
+    subtitle: 'Intermerdaire (4 Mois)',
+    price: '150€',
+    subPrice: '(3 * 150€ / 3 mois) ',
+    features: [
+      'Pack adapter aux initiés ayant déja des bases en musculation',
+      'Programme de musculation personalisé en fonction de tes objectifs. ',
+      'Plan alimentaire adapté à tes besoins. Accompagnement sur les divers TCA ou problématique de santé.',
+      'Bilan mensuel photo + mensuration avec un suivi plus approfondie',
+      'Communication whatsApp & Email + visio ou call chaque mois',
+    ],
+  },
+  {
+    title: 'Programmation niveau',
+    subtitle: 'Confirmé (4 Mois)',
+    price: '150€',
+    subPrice: '(3 * 150€ / 3 mois) ',
+    features: [
+      'Pack adapter aux initiés ayant déja des bases en musculation depuis plusieurs années',
+      'Programme de musculation personalisé en fonction de tes objectifs. ',
+      'Plan alimentaire adapté à tes besoins. Accompagnement sur les divers TCA ou problématique de santé.',
+      'Bilan mensuel photo + mensuration avec un suivi plus approfondie',
+      'Communication whatsApp & Email + visio ou call chaque mois',
+    ],
+  },
+  {
+    title: 'Programmation niveau',
+    subtitle: 'Débutant(1 Mois)',
+    price: '150€',
+    subPrice: '(1 * 150€ / 1 mois) ',
+    features: [
+      'Pack adapter aux débutants en musculation.',
+      'Programme de musculation personalisé en fonction de tes objectifs.',
+      'Plan alimentaire adapté à tes besoins',
+      'Accompagnement sur les divers TCA ou problématique de santé.',
+      'Bilan mensuel par email après 4 semaines',
+    ],
+  },
+  {
+    title: 'Programmation niveau',
+    subtitle: 'Intermerdaire (1 Mois)',
+    price: '150€',
+    subPrice: '(1 * 150€ / 1 mois) ',
+    features: [
+      'Pack adapter aux initiés ayant déja des bases en musculation',
+      'Programme de musculation personalisé en fonction de tes objectifs. ',
+      'Plan alimentaire adapté à tes besoins.',
+      'Accompagnement sur les divers TCA ou problématique de santé.',
+      'Bilan mensuel par email après 4 semaines',
+    ],
+  },
+  {
+    title: 'Programmation niveau',
+    subtitle: 'Confirmé (1 Mois)',
+    price: '150€',
+    subPrice: '(1 * 150€ / 1 mois) ',
+    features: [
+      'Pack adapter aux initiés ayant déja des bases en musculation',
+      'Programme de musculation personalisé en fonction de tes objectifs. ',
+      'Plan alimentaire adapté à tes besoins.',
+      'Accompagnement sur les divers TCA ou problématique de santé.',
+      'Bilan mensuel par email après 4 semaines',
+    ],
+  },
+]
+
+const EbookOptions = [
+  {
+    title: 'Programmation niveau',
+    subtitle: 'Débutant(4 Mois)',
+    price: '150€',
+    subPrice: '(3 * 150€ / 3 mois) ',
+    features: [
+      'Pack adapter aux débutants en musculation.',
+      'Programme de musculation personalisé en fonction de tes objectifs.',
+      'Plan alimentaire adapté à tes besoins',
+      'Accompagnement sur les divers TCA ou problématique de santé.',
+      'Bilan mensuel photo + mensuration avec un suivi plus approfondie',
+      'Communication whatsApp & Email + visio ou call chaque mois',
+    ],
+  },
+  {
+    title: 'Programmation niveau',
+    subtitle: 'Débutant(4 Mois)',
+    price: '150€',
+    subPrice: '(3 * 150€ / 3 mois) ',
+    features: [
+      'Pack adapter aux débutants en musculation.',
+      'Programme de musculation personalisé en fonction de tes objectifs.',
+      'Plan alimentaire adapté à tes besoins',
+      'Accompagnement sur les divers TCA ou problématique de santé.',
+      'Bilan mensuel photo + mensuration avec un suivi plus approfondie',
+      'Communication whatsApp & Email + visio ou call chaque mois',
+    ],
+  },
+]
+
+function Pricing() {
+  return <div>Pricing</div>
+}
+
+export default Pricing
