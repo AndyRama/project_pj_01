35f15b5b246e10f1d9aaf7d3c19ea3c41612685a
Andy Ramaroson <andyramaroson@gmail.com>
2024-03-31 15:31:16 +0200

    (feat)[Blog] try to fix error replace (remove old code)

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,42 +1,22 @@
 import React from 'react'
 import SubHero from '../components/about/SubHero'
 import { allPosts } from 'contentlayer/generated'
-import Posts from '@/app/components/blog/category/Posts'
 import Subscribe from '@/app/components/Subscribe'
 
-const page = ({ params }) => {
-  const newTitle = params?.slug.replace('-', ' ')
-  let itemsTotal = 0,
-    items = null
+export const metadata = {
+  title: 'Blogs - Unlcoaching by Jérémy Prat',
+}
 
-  if (params?.slug) {
-    items = allPosts.filter((post) =>
-      post.categories.some(
-        (category) =>
-          category.title
-            .toLowerCase()
-            .trim()
-            .replace(/[^\w\s-]/g, '')
-            .replace(/[\s_-]+/g, '-')
-            .replace(/^-+|-+$/g, '') === params.slug,
-      ),
-    )
-    itemsTotal = items.length
-  }
+const page = ({ params }) => {
   return (
     <>
       <SubHero
         className="pt-52 pb-24 capitalize"
-        title={`${newTitle} (${itemsTotal})`}
-        subTitle="Category"
+        title="Recent Updates"
+        subTitle="My Blog"
       />
-      <Posts
-        className="pt-0 pb-52"
-        archive={true}
-        params={params}
-        itemsPerPage={6}
-      />
-      <Subscribe className="py-16 pt-64 lg:py-32 bg-orange-600" />
+
+      <Subscribe className="py-16 pt-64 lg:py-32 bg-orange-400" />
     </>
   )
 }
