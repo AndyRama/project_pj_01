1b4772235190ccc3dfa41b411806d20456bbd7a5
Andy Ramaroson <andyramaroson@gmail.com>
2024-04-15 19:40:27 +0200

    (feat) add contact form component and add image temporaire

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SubHero from '../components/about/SubHero'
 import Subscribe from '../components/Subscribe'
+import ContactForm from '../components/ContactForm'
 
 export const metadata = {
   title: 'Contact Page - Unlcoaching',
@@ -10,6 +11,7 @@ const page = () => {
   return (
     <>
       <SubHero className="pt-32" title="Contact" />
+      <ContactForm className="pt-64" />
       <Subscribe className="py-16 pt-32 lg:py-32" />
     </>
   )
