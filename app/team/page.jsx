e4327679eec34c943fce96a8e4b05638daa93390
Andy Ramaroson <andyramaroson@gmail.com>
2024-05-08 23:40:52 +0200

    (feat)[team] create component testimonial

--- app/team/page.jsx
+++ app/team/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import SubHero from '../components/about/SubHero'
 import Subscribe from '../components/Subscribe'
+import Testimonial from '../components/Testimonial'
 
 export const metadata = {
   title: 'Coeur de la Team - Unlcoaching',
@@ -10,6 +11,7 @@ const page = () => {
   return (
     <>
       <SubHero className="pt-32" title="Le coeur de la team" />
+      {/* <Testimonial /> */}
       <Subscribe className="py-16 pt-32 lg:py-32" />
     </>
   )
