4d15e15c9e641aea3da277f88f7af79b2496a118
Andy Ramaroson <andyramaroson@gmail.com>
2024-05-18 09:43:10 +0200

    (feat)[contact] finish component card contact - map - Princing  modele

--- app/components/CardContact.jsx
+++ app/components/CardContact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 import { motion } from 'framer-motion'
 import Link from 'next/link'
-import CardContact1 from 'app/components/CardContact1'
+import CardInfo from '@/app/components/CardInfo'
 import CardMap from 'app/components/CardMap'
 import CardPricing from 'app/components/CardPricing'
 
@@ -12,7 +12,7 @@ const CardContact = ({ className }) => (
     <div className="container mx-auto mt-[-30] lg:mt-[-50px]">
       <div className="flex justify-around">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-auto">
-          <CardContact1 className=" w-12/12 lg:w-5/12 px-0 lg:px-4 xl:px-16 mb-10" />
+          <CardInfo className=" w-12/12 lg:w-5/12 px-0 lg:px-4 xl:px-16 mb-10" />
           <CardMap className="w-12/12 lg:w-5/12 px-0 lg:px-4 xl:px-16 mb-10" />
           <CardPricing className="w-12/12 lg:w-5/12 px-0 lg:px-4 xl:px-16 mb-10" />
         </div>
