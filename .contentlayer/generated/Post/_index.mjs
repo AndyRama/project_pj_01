89e5293c06fbf5c99cca48b731cd8286ec437614
andy ramaroson <andyramaroson@gmail.com>
2024-11-17 18:51:20 +0100

    (content)[Post] update article 1 et 2

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -3,12 +3,12 @@
 import blog__commentGarderUnBonMindsetMdx from './blog__Comment-garder-un-bon-mindset.mdx.json' assert { type: 'json' }
 import blog__commentPerdreDuPoidsMdx from './blog__Comment-perdre-du-poids.mdx.json' assert { type: 'json' }
 import blog__laCoursePourCardioMdx from './blog__La-course-pour-cardio.mdx.json' assert { type: 'json' }
-import blog__laSalleDeSportMdx from './blog__La-salle-de-sport.mdx.json' assert { type: 'json' }
 import blog__la_salle_de_sport_quand_on_debuteMdx from './blog__La_salle_de_sport_quand_on_debute.mdx.json' assert { type: 'json' }
 import blog__leSommeilMdx from './blog__Le-sommeil.mdx.json' assert { type: 'json' }
 import blog__lesProduitsPourNousAiderMdx from './blog__Les-produits-pour-nous-aider.mdx.json' assert { type: 'json' }
 import blog__uneBonneAlimentationMdx from './blog__Une-bonne-alimentation.mdx.json' assert { type: 'json' }
 import blog__leTempsDeReposMdx from './blog__le-temps-de-repos.mdx.json' assert { type: 'json' }
 import blog__test_4Mdx from './blog__test-4.mdx.json' assert { type: 'json' }
+import blog__comment_perdre_du_poids_de_maniere_saine_et_durableMdx from './blog__Comment_perdre_du_poids_de_maniere_saine_et_durable.mdx.json' assert { type: 'json' }
 
-export const allPosts = [blog__commentGarderUnBonMindsetMdx, blog__commentPerdreDuPoidsMdx, blog__laCoursePourCardioMdx, blog__laSalleDeSportMdx, blog__la_salle_de_sport_quand_on_debuteMdx, blog__leSommeilMdx, blog__lesProduitsPourNousAiderMdx, blog__uneBonneAlimentationMdx, blog__leTempsDeReposMdx, blog__test_4Mdx]
+export const allPosts = [blog__commentGarderUnBonMindsetMdx, blog__commentPerdreDuPoidsMdx, blog__laCoursePourCardioMdx, blog__la_salle_de_sport_quand_on_debuteMdx, blog__leSommeilMdx, blog__lesProduitsPourNousAiderMdx, blog__uneBonneAlimentationMdx, blog__leTempsDeReposMdx, blog__test_4Mdx, blog__comment_perdre_du_poids_de_maniere_saine_et_durableMdx]
